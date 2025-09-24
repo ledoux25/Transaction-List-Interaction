b0da23943c80d827f2e045776cda34b451f65596
ledoux25 <sanguojoseph17@gmail.com>
2025-09-24 12:53:28 -0400

    Initial Commit

---
+++ TrasactionListInteraction/TrasactionListInteractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrasactionListInteractionApp.swift
+//  TrasactionListInteraction
+//
+//  Created by Sanguo Joseph Ledoux on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TrasactionListInteractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrasactionListInteractionTests/TrasactionListInteractionTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrasactionListInteractionTests.swift
+//  TrasactionListInteractionTests
+//
+//  Created by Sanguo Joseph Ledoux on 9/24/25.
+//
+
+import Testing
+@testable import TrasactionListInteraction
+
+struct TrasactionListInteractionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
